3274da4ee0ade5cb120d99f6efa4957aecdace2e
yxc <yxc@meitu.com>
2018-08-19 21:20:33 +0800

    Initial commit

---
+++ app/src/main/java/tina/com/tinaalipayplugin/MainActivity.java
@@ -0,0 +1,13 @@
+package tina.com.tinaalipayplugin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
